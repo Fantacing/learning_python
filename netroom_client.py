c96355b3e74a84904f902c24c246cc711cfc60ed
fantacing <578923779@qq.com>
2016-04-20 17:12:58 +0800

    modify netroom

--- netroom_client.py
+++ netroom_client.py
@@ -1,4 +1,4 @@
-#-*- coding:utf-8 -*-
+﻿#-*- coding:utf-8 -*-
 import socket
 import time
 import threading
@@ -7,22 +7,36 @@ HOST = "127.0.0.1"
 PORT = 9988
 ADDR = (HOST, PORT)
 
+# 返回当前时间
+def get_datetime():
+    format = '%Y-%m-%d %H:%M:%S'
+    value = time.localtime()
+    dt = time.strftime(format, value)
+    return str(dt)
 
+# 接收用户输入
 def send_msg(s):
     while True:
         msg = raw_input()
         s.send(msg)
 
-
+# 接收服务器端消息
 def get_msg(s):
     while True:
-        reply = s.recv(1024)
-        print(reply)
+        try:
+            reply = s.recv(1024)
+            print("[{0}] {1}".format(get_datetime(),reply))
+        except socket.error, e:
+            print(u"与服务器断开连接...")
+            s.close()
+            break
 
-c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-c_socket.connect(ADDR)
 
-t1 = threading.Thread(target=send_msg, args=(c_socket,))
-t2 = threading.Thread(target=get_msg, args=(c_socket,))
-t1.start()
-t2.start()
+if __name__ == '__main__':
+    c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    c_socket.connect(ADDR)
+
+    t1 = threading.Thread(target=send_msg, args=(c_socket,))
+    t2 = threading.Thread(target=get_msg, args=(c_socket,))
+    t1.start()
+    t2.start()
--- netroom_server.py
+++ netroom_server.py
@@ -1,6 +1,5 @@
-#-*- coding:utf-8 -*-
+﻿#-*- coding:utf-8 -*-
 import socket
-import time
 import threading
 
 HOST = ""
@@ -10,16 +9,8 @@ CONNECTNUM = 10
 ADDR = (HOST, PORT)
 c_sockets = []
 
-# 返回当前时间
 
-
-def get_datetime():
-    format = '%Y-%m-%d %H:%M:%S'
-    value = time.localtime()
-    dt = time.strftime(format, value)
-    return dt
-
-
+# 接收客户端消息 并返回给客户端
 def get_msg(s, addr):
     while True:
         try:
@@ -32,18 +23,18 @@ def get_msg(s, addr):
             for sock in c_sockets:
                 try:
                     sock.send(
-                        "[{0}] 用户{1}:{2}".format(get_datetime(), addr[0], recv))
+                        "{0} : {1}".format(str(addr[0]), str(recv)))
                 except socket.error, e:
                     del sock
 
+if __name__ == '__main__':
+    s_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s_socket.bind(ADDR)
+    s_socket.listen(CONNECTNUM)
 
-s_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s_socket.bind(ADDR)
-s_socket.listen(CONNECTNUM)
-
-while True:
-    c_socket, addr = s_socket.accept()
-    print("{0}加入房间...".format(addr[0]))
-    c_sockets.append(c_socket)
-    t1 = threading.Thread(target=get_msg, args=(c_socket, addr))
-    t1.start()
+    while True:
+        c_socket, addr = s_socket.accept()
+        print(u"{0}加入房间...".format(addr[0]))
+        c_sockets.append(c_socket)
+        t1 = threading.Thread(target=get_msg, args=(c_socket, addr))
+        t1.start()
